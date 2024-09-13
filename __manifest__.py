2aabfbfa3ef716bf289809b2616aa8b808c71a9d
Herbert.shu <herbert.shu@bwcs.com.cn>
2024-09-13 10:22:55 +0800

    剩下所有资料夹都被跟踪-2

---
+++ __manifest__.py
@@ -0,0 +1,14 @@
+{
+    'name': '文档',
+    'description': '文档',
+    'author': 'bcs_pmo_herbert',
+    'version': '1.0',
+    'category': 'Uncategorized',
+    'website': 'http://www.example.com',
+    'depends': ['base'],
+    'data': [
+        'views/bcs_document_view.xml',
+        'security/ir.model.access.csv',
+    ],
+    'application': True,
+}
